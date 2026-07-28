3e6487378fa3bbe493832e63e50c190cecfcc5ae
COLTELLINO <filippo.patrignani2@studio.unibo.it>
2026-07-28 14:28:33 +0200

    faretra forced

--- sbatch_script.sh
+++ sbatch_script.sh
@@ -2,4 +2,4 @@
 
 export WANDB_API_KEY="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"
 
-sbatch -N 1 --gpus=nvidia_geforce_rtx_3090:1 run_docker.sh "Qwen/Qwen3-0.6B" "ShenLab/MentalChat16K"
+sbatch -N 1 --gpus=nvidia_geforce_rtx_3090:1 -w faretra run_docker.sh "Qwen/Qwen3-0.6B" "ShenLab/MentalChat16K"
