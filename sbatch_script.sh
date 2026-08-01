a2dc7436fb2b8db625d676f6c2c6de3944222a19
COLTELLINO <filippo.patrignani2@studio.unibo.it>
2026-08-01 16:44:58 +0200

    pin sbatch to faretra by default

--- sbatch_script.sh
+++ sbatch_script.sh
@@ -6,4 +6,4 @@ export WANDB_API_KEY="wandb_v1_2vKOf5nieTGSiAgqatMTjK6cVXd_HhJrz2yDtcE0xMFPQpHXO
 # google/medgemma-4b-it, google/gemma-3-4b-it). Accept each model's license
 # on huggingface.co first, then export HF_TOKEN="hf_..." before running this.
 
-sbatch -N 1 --gpus=nvidia_geforce_rtx_3090:1 run_docker.sh
+sbatch -w faretra -N 1 --gpus=nvidia_geforce_rtx_3090:1 run_docker.sh
